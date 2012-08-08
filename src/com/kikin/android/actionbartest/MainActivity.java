c8b271cd0904a2908508bf7e69cf85a0f9e9d563
Francisco S Velazquez <fvelazquez@ign.com>
2012-08-08 02:14:22 -0700

    initial commit

---
+++ src/com/kikin/android/actionbartest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.kikin.android.actionbartest;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.RenderPriority;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends SherlockActivity {
+
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String url = new String("http://widgets.ign.com/global/wikis/wikimap.html?full=true&controls=false&?popup=iphone");
+        setContentView(R.layout.activity_main);
+        
+        // Obtain the webView by ID
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // performance hacks!
+        webSettings.setRenderPriority(RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        
+        // multi-touch zoom
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        //myWebView.loadUrl("http://widgets.ign.com/global/wikis/wikimap.html?full=true&?popup=iphone");
+        myWebView.loadUrl(url);
+        myWebView.setWebViewClient(new WebViewClient());
+        
+    }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.maps, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
+}
